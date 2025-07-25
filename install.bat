6d82dcf3161e38b102a19d2d21378fcfe712b92b
hak3du <uhack8521@gmail.com>
2025-07-25 06:11:55 -0700

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo 🚀 Setting up AHE SDK environment...
-python -m venv ahe_env
-call ahe_env\Scripts\activate
-pip install --upgrade pip
-pip install -r requirements.txt
-echo ✅ Installation complete. To activate: call ahe_env\Scripts\activate
-pause
